17db0cf7e36e08a047119886201478bfed4a3f91
ZhugeXican <chenxican@aliyun.com>
2019-06-12 17:34:43 +0800

    更新二级文件html读取不到以及没有script标签无法复制的问题

--- changeMod.js
+++ changeMod.js
@@ -12,9 +12,8 @@ const async = require('async');
 		  }
 
 		  paths.forEach(function(path){
-				if(path.indexOf('.html')!==-1){
-					return;
-				}
+			 var srcOld=src;
+			 var dstOld=dst;
 			  var _src=src+'/'+path;
 			  var _dst=dst+'/'+path;
 			  var readable;
@@ -23,7 +22,10 @@ const async = require('async');
 				  if(err){
 					  throw err;
 				  }
-				  
+				  if(path.indexOf('.html')!==-1){
+					  copyHtml(srcOld,dstOld,path)
+					return;
+				}
 				  if(st.isFile()){
 					  readable=fs.createReadStream(_src);//创建读取流
 					  writable=fs.createWriteStream(_dst);//创建写入流
@@ -53,36 +55,42 @@ const async = require('async');
 
 
 
-  
-const files = fs.readdirSync('./project').filter(function (file) {
-	return Path.extname(file).toLowerCase() === '.html';
-  });
+var copyHtml=function(srcOld,dstOld,path){
 
-  if(!fs.existsSync('./First')){
-	fs.mkdirSync('First')
-}
-
-var reg=/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
-  for(var i=0;i<files.length;i++){
-	var data = fs.readFileSync(`./project/${files[i]}`, 'utf8');
-	var regData=data.match(reg)
-	if(regData){
-		var inserFirst=`<script>if (typeof module === 'object') {window.module = module; module = undefined;}</script>${regData[0]}`
-		var inserEnd=`${regData[regData.length-1]} <script>if (window.module) module = window.module;</script>`
-		var reg1=new RegExp(regData[0])
-		var reg2=new RegExp(regData[regData.length-1])
-		var data1=data.replace(reg1,inserFirst)
-		var data2=data1.replace(reg2,inserEnd)
-		fs.writeFile(`./First/${files[i]}`,data2,'utf8',function(error){
-			if(error){
-				console.log(error);
-				return false;
-			}
-		})
 	
+	  if(!fs.existsSync('./First')){
+		fs.mkdirSync('First')
 	}
-  }
-
+	
+	var reg=/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi;
+	 
+		var data = fs.readFileSync(`${srcOld}/${path}`, 'utf8');
+		var regData=data.match(reg)
+		if(regData){
+			var inserFirst=`<script>if (typeof module === 'object') {window.module = module; module = undefined;}</script>${regData[0]}`
+			var inserEnd=`${regData[regData.length-1]} <script>if (window.module) module = window.module;</script>`
+			var reg1=new RegExp(regData[0])
+			var reg2=new RegExp(regData[regData.length-1])
+			var data1=data.replace(reg1,inserFirst)
+			var data2=data1.replace(reg2,inserEnd)
+			fs.writeFile(`${dstOld}/${path}`,data2,'utf8',function(error){
+				if(error){
+					console.log(error);
+					return false;
+				}
+			})
+		}else {
+			fs.writeFile(`${dstOld}/${path}`,data,'utf8',function(error){
+				if(error){
+					console.log(error);
+					return false;
+				}
+			})
+		}
+	  
+	
+}
+  
 
 // var data = fs.readFileSync('./\*.html', 'utf8');
 // console.log(data);
